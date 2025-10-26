c9c4d3456ca853446f85d29dfd2ec548c2543e4e
Sapana Prajapati <sapana.prajapati2505@gmail.com>
2025-10-26 20:17:49 +0530

    Add Backend Link In Frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,23 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080', 
+      '/api': 'http://localhost:8080',
     },
     port: 3000,
   },
   build: {
     outDir: 'dist',
+    chunkSizeWarningLimit: 3000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('chart.js')) return 'chartjs';
+            if (id.includes('highlight.js')) return 'highlightjs';
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
 });
