9f77511a416f1645c0ee53ae4db7b30aeced65f7
Sapana Prajapati <sapana.prajapati2505@gmail.com>
2025-10-27 19:12:24 +0530

    Update Vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -12,26 +11,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 2000,
-    sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('chart.js')) return 'chartjs';
-            if (id.includes('highlight.js')) return 'highlightjs';
-            if (id.includes('react')) return 'react';
-            return 'vendor';
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 3000,
   },
 });
